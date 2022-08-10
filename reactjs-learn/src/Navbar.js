4e5d688be7ec77b39c5ce044a73d099b39abb81f
umutdurna <umutdurna@sameup.co>
2022-08-10 13:55:59 +0300

    multiple-components-Navbar-and-Home-added
    multiple-components-Navbar-and-Home-added (Komponentler eklendi)

---
+++ reactjs-learn/src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Yeni Dünyaya Açılan Blog</h1>
+            <div className="links">
+                <a href="/">Ev</a>
+                <a href="/create">Yeni Blog</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
