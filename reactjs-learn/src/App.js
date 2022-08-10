2a0099190864f03b4a98cf986a8e01566171db2a
umutdurna <umutdurna@sameup.co>
2022-08-10 12:42:43 +0300

    tutorial-first-commit

--- reactjs-learn/src/App.js
+++ reactjs-learn/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const title = "React Öğrenme Yerimize Hoş Geldiniz";
+  const likes = 50;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{ title }</h1>
+        <p>{ likes } Kez Beğenildi.</p>
+      </div>
     </div>
   );
 }
